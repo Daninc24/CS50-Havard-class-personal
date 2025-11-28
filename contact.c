747549c7663a5ecafe7753af311ecc944b1ee524
Daninc24 <danmailur8@gmail.com>
2025-11-28 11:46:51 +0300

    getting strings and integers without using cs50 and adding info to external file like contacts

---
+++ contact.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *file = fopen("contact.csv", "a");
+    
+    string name = get_string("Name: ");
+    string number = get_string("Number: ");
+
+    fprintf(file, "%s,%s\n", name, number);
+    fclose(file);
+    return 0;
+    
+}
