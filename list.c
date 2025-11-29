87d73a701e16074d2a08f3d4ac21c8aa9551fcdc
Daninc24 <danmailur8@gmail.com>
2025-11-29 12:11:12 +0300

    reallocation of memory when resizing the array

--- list.c
+++ list.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    int list[3];
+    int *list = malloc(3 * sizeof(int));
+    if (list == NULL)
+    {
+        return 1;
+    }
+    list[0] = 10;
+    list[1] = 20;
+    list[2] = 30;
 
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
+    //time passes need more memory
 
-    for (int i = 0; i < 3; i++)
+    int *tmp = realloc(list, 4 * sizeof(int));
+    if (tmp == NULL)
+    {
+        free(list);
+        return 1;
+    }
+    tmp[3] = 40;
+
+    free(list);
+    list = tmp;
+
+    for (int i = 0; i < 4; i++)
     {
         printf("%d\n", list[i]);
-        
     }
-
 }
